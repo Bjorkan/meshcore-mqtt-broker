37e58668deaa6010206e4c01391511b305de2acb
Björkan <git.lunacy614@passmail.com>
2026-06-17 22:50:04 +0200

    Fix argument forwarding in docker-entrypoint.sh för su

--- broker/docker-entrypoint.sh
+++ broker/docker-entrypoint.sh
@@ -6,7 +6,7 @@ DATA_DIR="${ABUSE_DATA_DIR:-/data}"
 if [ "$(id -u)" = "0" ]; then
   mkdir -p "$DATA_DIR"
   chown -R node:node "$DATA_DIR"
-  exec su node -s /bin/sh -c 'exec "$@"' -- "$@"
+  exec su node -s /bin/sh -c 'exec "$@"' -- sh "$@"
 fi
 
 exec "$@"
